61d96b85b65b8ddd330458835db391a7821b6ae4
Matheus Filipe T. Chaves <matheus.02468@gmail.com>
2017-02-20 16:26:56 -0500

    Adjusted question view.

--- public/controllers/question.js
+++ public/controllers/question.js
@@ -4,10 +4,32 @@ var app = angular.module("qbApp");
 
 app.controller("questionController", function($scope, $routeParams, Group, Question) {
 
-  $scope.selectedGroup;
-  $scope.started;
+  $scope.group;
+  $scope.questions;
+  $scope.question;
+  $scope.started = false;
 
   Group.findById($routeParams._groupId).then(function(response) {
-    $scope.selectedGroup = response.data.record;
+    $scope.group = response.data.record;
   });
+
+  Question.findByGroup($routeParams._groupId).then(function(response) {
+    $scope.questions = response.data;
+
+    $scope.questions.forEach(function(question, index) {
+      question.order = ++index;
+      question.isAnswered = false;
+    });
+  });
+
+  $scope.start = function() {
+    $scope.started = true;
+    $scope.question = $scope.getNextQuestion();
+  };
+
+  $scope.getNextQuestion = function() {
+    return $scope.questions.find(function(question) {
+      return question.isAnswered === false;
+    });
+  };
 });
