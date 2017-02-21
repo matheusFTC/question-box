f0b3f3c2533417440cabb804f51b77e79dbc4fa1
Matheus Filipe T. Chaves <matheus.02468@gmail.com>
2017-02-21 15:57:08 -0500

    Adjusted question view.

--- public/controllers/question.js
+++ public/controllers/question.js
@@ -16,20 +16,35 @@ app.controller("questionController", function($scope, $routeParams, Group, Quest
   Question.findByGroup($routeParams._groupId).then(function(response) {
     $scope.questions = response.data;
 
+    const alphabet = ["A", "B", "C", "D", "E", "F", "G", "I", "J", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Z"];
+
     $scope.questions.forEach(function(question, index) {
       question.order = ++index;
       question.isAnswered = false;
+
+      question.alternatives.forEach(function(alternative, index) {
+        alternative.order = alphabet[index];
+        alternative.isMarcked = false;
+      });
     });
   });
-
-  $scope.start = function() {
-    $scope.started = true;
-    $scope.question = $scope.getNextQuestion();
-  };
 
   $scope.getNextQuestion = function() {
     return $scope.questions.find(function(question) {
       return question.isAnswered === false;
     });
+  };
+  
+  $scope.start = function() {
+    $scope.started = true;
+    $scope.question = $scope.getNextQuestion();
+  };
+
+  $scope.mark = function(alternative) {
+    alternative.isMarcked = !alternative.isMarcked;
+  };
+  
+  $scope.next = function() {
+    
   };
 });
