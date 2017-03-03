31898a44d8d52e8dc422d1f4e203254bd35b3abc
Matheus Filipe T. Chaves <matheus.02468@gmail.com>
2017-03-03 07:38:50 -0500

    Adjusted authentication view.

--- public/controllers/authentication.js
+++ public/controllers/authentication.js
@@ -2,19 +2,29 @@
 
 var app = angular.module("qbApp");
 
-app.controller("authenticationController", function($rootScope, $scope, Authentication) {
+app.controller("authenticationController", function($rootScope, $scope, $location, Authentication) {
 
   $scope.message = null;
   $scope.username = null;
   $scope.password = null;
 
   $scope.authenticate = function() {
-    Authentication.authenticate($scope.username, $scope.password)
-      .then(function(response) {
-        $rootScope.token = response.data.token;
-      })
-      .catch(function(err) {
-        
-      });
+    if ($scope.username === null || $scope.password === null || $scope.username.trim() === "" || $scope.password.trim() === "") {
+      $scope.message = "Informe o usuário e a senha.";
+    } else {
+      Authentication.authenticate($scope.username, $scope.password)
+        .then(function(response) {
+          $rootScope.token = response.data.token;
+
+          $location.path("/");
+        })
+        .catch(function(err) {
+          if (err.status === 401) {
+            $scope.message = "Usuário e/ou senha invalído(s).";
+          } else {
+            $scope.message = "Não conseguimos realizar a sua autenticação.";
+          }
+        });
+    }
   };
 });
