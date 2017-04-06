f31c00d3b248fe775f7fac7fcf5ff199b07c97a6
Matheus Filipe T. Chaves <matheus.02468@gmail.com>
2017-04-06 13:11:13 -0400

    Adjusted messege.

--- public/app.js
+++ public/app.js
@@ -10,36 +10,49 @@ app.config(function($routeProvider) {
 });
 
 app.run(function($rootScope) {
-  
+
   $rootScope.message = {
     text: null,
     isSuccess: false,
     isError: false,
     success: function(text) {
+      this.clear();
+
       this.text = text;
       this.isSuccess = true;
     },
     error: function(text) {
+      this.clear();
+
       this.text = text;
       this.isError = true;
     },
     internalServerError: function(text) {
+      this.clear();
+
       if (text) {
         this.text = text;
       } else {
         this.text = "This action could not be taken.";
       }
+
       this.isError = true;
     },
     unauthorized: function(text) {
+      this.clear();
+
       if (text) {
         this.text = text;
       } else {
         this.text = "You are not authorized to perform this action.";
       }
+
       this.isError = true;
     },
     close: function() {
+      this.clear();
+    },
+    clear: function() {
       this.text = null;
       this.isSuccess = false;
       this.isError = false;
