9fd8d9c6839451f9788701eb68747635c506dab4
Matheus Filipe T. Chaves <matheus.02468@gmail.com>
2017-04-19 16:37:03 -0400

    Added i18n.

--- public/app.js
+++ public/app.js
@@ -1,15 +1,49 @@
 "use strict";
 
-var app = angular.module("qbApp", ["ngRoute", "ngCookies"]);
+var app = angular.module("qbApp", ["ngRoute", "ngCookies", "pascalprecht.translate"]);
 
-app.config(function($routeProvider) {
+app.config(function($routeProvider, $translateProvider) {
 
   $routeProvider.otherwise({
     redirectTo: "/"
   });
+  
+  $translateProvider.translations("en", {
+    "BTN_EXIT": "Exit",
+    "BTN_FINISH": "Finish",
+    "BTN_HOME": "Home",
+    "BTN_PREVIOUS": "Previous",
+    "BTN_NEXT": "Next",
+    "BTN_REMAKE": "Remake",
+    "BTN_REVIEW": "Review",
+    "BTN_START": "Start",
+    "ERROR_LOAD_GROUP": "Something went wrong, try again...",
+    "INTERNAL_SERVER_ERROR": "This action could not be taken.",
+    "GROUP_FILTER": "Do you look for which group?",
+    "TITLE": "Question Box",
+    "UNAUTHORIZED": "You are not authorized to perform this action."
+  });
+ 
+  $translateProvider.translations("pt", {
+    "BTN_EXIT": "Sair",
+    "BTN_FINISH": "Finalizar",
+    "BTN_HOME": "Início",
+    "BTN_PREVIOUS": "Anterior",
+    "BTN_NEXT": "Próximo",
+    "BTN_REMAKE": "Refazer",
+    "BTN_REVIEW": "Revisar",
+    "BTN_START": "Iniciar",
+    "ERROR_LOAD_GROUP": "Algo está errado, tente outra vez...",
+    "INTERNAL_SERVER_ERROR": "Não foi possível concluir esta ação.",
+    "GROUP_FILTER": "Você procura por qual grupo?",
+    "TITLE": "Caixa de Questões",
+    "UNAUTHORIZED": "Você não possui permissão para esta ação."
+  });
+ 
+  $translateProvider.preferredLanguage("pt");
 });
 
-app.run(function($rootScope) {
+app.run(function($rootScope, $translate) {
 
   $rootScope.message = {
     text: null,
@@ -33,7 +67,7 @@ app.run(function($rootScope) {
       if (text) {
         this.text = text;
       } else {
-        this.text = "This action could not be taken.";
+        this.text = $translate("INTERNAL_SERVER_ERROR");
       }
 
       this.isError = true;
@@ -44,7 +78,7 @@ app.run(function($rootScope) {
       if (text) {
         this.text = text;
       } else {
-        this.text = "You are not authorized to perform this action.";
+        this.text = $translate("UNAUTHORIZED");
       }
 
       this.isError = true;
@@ -56,6 +90,12 @@ app.run(function($rootScope) {
       this.text = null;
       this.isSuccess = false;
       this.isError = false;
+    }
+  };
+  
+  $rootScope.language = {
+    change: function(key) {
+      $translate.use(key);
     }
   };
 });
